46e9d75ee0a778d91c11e96b5e771ccb8cdb71e4
Marcus Weaver <onemangd@gmail.com>
2017-09-13 01:13:20 +0300

    Add files via upload

---
+++ HonorCup2/Filter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Numerics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorCup2
+{
+    internal class Filter
+    {
+        private const double K = 512;
+        
+        /// <summary>Возвращает сетку частот</summary>
+        public static double[] FrequencyGrid ()
+        {
+            var grid = new double[512];
+            for (int i = 0; i < 512; i++)
+            {
+                grid[i] = (i * Math.PI) / K;
+                Console.WriteLine($"{grid[i]} {i}");
+            }
+            return grid;
+        }
+
+        /// <summary>Комплексный коэффициент передачи</summary>
+        public static Complex ComplexTransferСoefficient(double[] quants, double omegaFrequency)
+        {
+            var res = ComplexSumB(quants, omegaFrequency) 
+                     / (new Complex(1, 0) + ComplexSumA(quants, omegaFrequency));
+            return res;
+        }
+
+        /// <summary>Комплексная сумма для b (начинается с нуля)</summary>
+        public static Complex ComplexSumB(double[] quants, double omegaFrequency)
+        {
+            var sum = new Complex(0, 0);
+            for (int i = 0; i < quants.Length; i++)
+            {
+                var quantComplex = new Complex(quants[i], 0);
+                var complexExponent = Complex.Exp(new Complex(0, omegaFrequency * i * -1));
+                sum += quantComplex * complexExponent;
+            }
+            return sum;
+        }
+
+        /// <summary>Комплексная сумма для a (начинается с единицы)</summary>
+        public static Complex ComplexSumA(double[] quants, double omegaFrequency)
+        {
+            var sum = new Complex(0, 0);
+            for (int i = 1; i <= quants.Length; i++)
+            {
+                var quantComplex = new Complex(quants[i - 1], 0);
+                var complexExponent = Complex.Exp(new Complex(0, omegaFrequency * i * -1));
+                sum += quantComplex * complexExponent; //Complex.Multiply(quantComplex, complexExponent);
+            }
+            return sum;
+        }
+
+        //Mean Square of Error must be here
+    }
+}
---
+++ HonorCup2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HonorCup2
+{
+    class Program
+    {
+        static void Main()
+        {
+            var inputB = Console.ReadLine()?.Split(',');
+            var inputA = Console.ReadLine()?.Split(',');
+
+            IEnumerable<double> InputToNumbers(IEnumerable<string> input)
+            {
+                var nums = new List<double>();
+                foreach (var i in input)
+                {
+                    if (double.TryParse(i, NumberStyles.Number, CultureInfo.InvariantCulture, out var num))
+                        nums.Add(num);
+                    else
+                        Console.WriteLine("Wrong Input!");
+                }
+                return nums;
+            }
+
+            var numbersA = InputToNumbers(inputA);
+            var numbersB = InputToNumbers(inputB);
+
+            foreach (var number in numbersA)
+            {
+                Console.WriteLine(number);
+            }
+
+            Filter.FrequencyGrid();
+
+            Console.ReadLine();
+        }
+    }
+}
